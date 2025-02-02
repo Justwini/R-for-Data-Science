7d3f2bb86e4949689d384bbd4d7f852c6803bb71
Justine <jujuarksx@gmail.com>
2025-02-02 23:01:08 +0100

    Add files via upload

---
+++ Data transformation.R
@@ -0,0 +1,34 @@
+library(nycflights13)
+library(tidyverse)
+install.packages("nycflights13")
+glimpse(flights)
+
+flights|>
+  filter(dest=="IAH") |>
+  group_by(year, month, day) |>
+  summarize(
+    arr_delay=mean(arr_delay, na.rm = TRUE)
+  )
+
+flights |> 
+  filter(dep_delay>120)
+
+flights|> 
+  filter(month == 1 & day == 1)
+
+flights |> 
+  filter(month == 1 | month == 2)
+
+flights |> 
+  filter(month %in% c(1, 2))
+jan1 <- flights |> 
+  filter(month == 1 & day == 1)
+flights |> 
+  arrange(year, month, day, dep_time)
+flights |> 
+  arrange(desc(dep_delay))
+flights |> 
+  distinct()
+
+flights |> 
+  distinct(origin, dest)
